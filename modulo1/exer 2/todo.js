a29ba6561abd6b9c598e49578abb8976cda9ca32
unknown <antonio_s_goncalves@saojose.sc.senai.br>
2019-06-17 12:25:25 -0300

    aaaaaaaaa

--- modulo1/exer 2/todo.js
+++ modulo1/exer 2/todo.js
@@ -1,1 +1,16 @@
-alert("fehsh")
+var listElement = 
+document.querySelector("#app ul")
+
+var inputElement = 
+document.querySelector("#app input")
+
+var buttonElement = 
+document.querySelector("#app button")
+
+var todos = [
+    'faze os bagui',
+    'slaaaa'
+]
+console.log(inputElement)
+console.log(listElement)
+console.log(buttonElement)
